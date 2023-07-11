c7f6510c1e034fb2774b02ed4362ff7bb67e0be3
parunkov <parunkov@gmail.com>
2023-07-11 12:38:23 +0500

    styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 function App() {
   return (
@@ -10,6 +7,21 @@ function App() {
       <div className="App">
         AAA
       </div>
+      <style>{`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+        
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+      `}</style>
     </>
   );
 }
