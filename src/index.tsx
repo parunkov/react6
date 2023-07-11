d83446a8eba8bde3c63ceaf781882510718928fd
parunkov <parunkov@gmail.com>
2023-07-11 12:30:48 +0500

    app created

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        AAA
+      </div>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
